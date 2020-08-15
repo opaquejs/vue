18bd54d9fce049d3be37337fe1af01ca5abc5566
Sebastian Dittrich <sebastian.dittrich@live.de>
2020-08-15 17:28:41 +0200

    fixed deep raw bug

--- src/makeReactiveStorage.ts
+++ src/makeReactiveStorage.ts
@@ -1,15 +1,18 @@
 import { reactive, toRaw } from 'vue'
-import { Constructor, Storage } from '@opaquejs/opaque'
+import { Constructor, Storage, Attributes } from '@opaquejs/opaque'
 
 export default <T extends Constructor<Storage>>(base: T) => class VueReactive extends base {
 
+    protected raw: Attributes<number>[]
+
     constructor(...args: any[]) {
         super(...args)
+        this.raw = super.all()
         this.replace(reactive(this.all()))
     }
 
     public all() {
-        return toRaw(super.all())
+        return this.raw
     }
 
 }
