b59a8482b1fb8ad267f55ce25058a47fda033931
Sebastian Dittrich <sebastian.dittrich@live.de>
2020-08-15 18:25:27 +0200

    fix: storage is no longer reactive

--- src/makeReactive.ts
+++ src/makeReactive.ts
@@ -6,7 +6,7 @@ import makeReactiveStorage from './makeReactiveStorage'
 export default <T extends Constructor<Model> & typeof Model>(base: T) => class VueReactiveModel extends base {
 
     protected static $query = makeReactiveQuery(base.$query)
-    protected static storage = makeReactiveStorage(base.storage);
+    // protected static storage = makeReactiveStorage(base.storage);
 
     constructor(...args: any[]) {
         super(...args)
