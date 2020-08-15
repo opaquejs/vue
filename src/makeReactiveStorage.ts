0b36017296cc1b935ef985c57f769272ac65dc44
Sebastian Dittrich <sebastian.dittrich@live.de>
2020-08-15 17:39:21 +0200

    fix: all storage returned observable

--- src/makeReactiveStorage.ts
+++ src/makeReactiveStorage.ts
@@ -3,16 +3,13 @@ import { Constructor, Storage, Attributes } from '@opaquejs/opaque'
 
 export default <T extends Constructor<Storage>>(base: T) => class VueReactive extends base {
 
-    protected raw: Attributes<number>[]
-
     constructor(...args: any[]) {
         super(...args)
-        this.raw = super.all()
         this.replace(reactive(this.all()))
     }
 
     public all() {
-        return this.raw
+        return toRaw(super.all()).map(document => toRaw(document))
     }
 
 }
