4af54474f1b77c305cced6d613784c7a25002500
Sebastian Dittrich <sebastian.dittrich@live.de>
2020-12-22 18:18:34 +0100

    Made storage optional to also make models reactive which have no storage

--- src/reactive.ts
+++ src/reactive.ts
@@ -2,7 +2,7 @@ import { OpaqueModel, Constructor, ModelAttributes } from '@opaquejs/opaque'
 import { reactive as vuereactive } from 'vue'
 import { reactiveStorage } from './reactiveStorage'
 
-export const reactive = <T extends Constructor<OpaqueModel> & { $adapterConstructor: Constructor<{ storage: Map<unknown, unknown> }> }>(base: T) => class VueReactiveModel extends base {
+export const reactive = <T extends Constructor<OpaqueModel> & { $adapterConstructor: Constructor<{ storage?: Map<unknown, unknown> }> }>(base: T) => class VueReactiveModel extends base {
     static $adapterConstructor = reactiveStorage(base.$adapterConstructor)
 
     constructor(...args: any[]) {
--- src/reactiveStorage.ts
+++ src/reactiveStorage.ts
@@ -1,11 +1,13 @@
 import { Constructor } from '@opaquejs/opaque'
 import { reactive } from 'vue'
 
-export const reactiveStorage = <T extends Constructor<{ storage: Map<unknown, unknown> }>>(base: T) => class ReactiveAdapter extends base {
+export const reactiveStorage = <T extends Constructor<{ storage?: Map<unknown, unknown> }>>(base: T) => class ReactiveAdapter extends base {
 
     constructor(...args: any[]) {
         super(...args)
-        this.storage = reactive(this.storage)
+        if (this.storage) {
+            this.storage = reactive(this.storage)
+        }
     }
 
 }
